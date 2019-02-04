7842a8d13d60d73d620701984ecfd647d3caecaa
AnnaKom <44025588+AnnaKom@users.noreply.github.com>
2019-02-04 22:47:08 +0300

    Add files via upload

--- Lab3_2/child.js
+++ Lab3_2/child.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const settings = require('./config.json');
 process.on('message', (obj) => { // obj – переменная содержащая объект отправленный родителем
-		logData = (new Date()).toString()+ " " + obj.metod + " " + obj.params + "/n";//Код обработки сообщений от родительского процесса 
+		logData = (new Date()).toString()+ " " + obj.method + " " + obj.params + "/n";//Код обработки сообщений от родительского процесса 
 		fs.writeFile(settings.logFile, logData, {
 			encoding:'utf8',
 				flag:'a'
